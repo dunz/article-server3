9b0be0f86b9713d803348acc886f0c9faea4f189
dunz <zergcake@gmail.com>
2022-04-02 16:09:38 +0900

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6cdaa1070006432747086680cc671fae'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'eb81a841-66e6-48a7-a6b2-f46ec7ec9281'
+};
